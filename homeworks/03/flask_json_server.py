4476cc0058d9148ad2ee0d55be764891c62cf049
Evgeny Tumanovsky <kl11mnkl11mn@gmail.com>
2018-10-28 13:31:42 +0300

    flask server

--- homeworks/03/flask_json_server.py
+++ homeworks/03/flask_json_server.py
@@ -4,14 +4,24 @@ app = Flask(__name__)
 
 @app.route("/get_classifier_result/<version>", methods=['GET', 'POST'])
 def return_classifier_result(version):
-    #TODO прочитать из полученного запроса json-контент
-    #В случае, если version==1, то должен вернуться json с версией и полем predict из входящего jsonа {"version":1, "predict":<predict_value>}
-    #В случае, если version==0, то должен вернуться json с версией и полем old_predict из входящего jsonа {"version":0, "predict":<old_predict_value>}
+    if request.method == 'POST':
+        if int(version) == 1:
+            return jsonify(version = 1, predict = request.get_json()['predict'])
+        else:
+            return jsonify(version = 0, predict = request.get_json()['old_predict'])
 
 @app.route("/")
 def hello():
-    #TODO должна возвращатьс инструкция по работе с сервером
-    return
+    return """Привет я flask сервер
+    Отправь мне POST запрос на url /get_classifier_result/<version>
+    Если хочешь использовать старую версию, то подставь 0,
+    а если новую, то 1 ;)
+    Также отправь мне json в формате 
+    {"predict" : i} для новой версии и 
+    {"old_predict": i} для старой.
+    В ответ я тоже отправлю тебе json в таком формате:
+    {"version" : value, "predict" : value}
+    """
 
 if __name__ == "__main__":
     app.run()
